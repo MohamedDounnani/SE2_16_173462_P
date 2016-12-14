8e470bf8193c9229c8d0f38ecdd05bcf0282827b
mohamed dounnani <mohamed dounnani>
2016-12-14 11:02:48 +0100

    Aggiunti ulteriori casi di test

--- spec/projectspec.js
+++ spec/projectspec.js
@@ -1,11 +1,19 @@
 var request = require("request");
-var base_url1 = "https://infinite-coast-33422.herokuapp.com/"
-var base_url2 = "https://infinite-coast-33422.herokuapp.com/registrazione"
+var bind = require('bind');
+var base_url = "https://infinite-coast-33422.herokuapp.com"
+var Urls = [];
+Urls.push('/registrazione');
+Urls.push('/login_home');
+Urls.push('/logout');
+Urls.push('/prenotazione');
+
+
+
 
 describe("Project Test", function(){    
     describe("GET /", function() {   
         it("returns status code 200", function(done) {     
-            request.get(base_url1, function(error, response, body) {
+            request.get(base_url, function(error, response, body) {
             expect(response.statusCode).toBe(200);
             done();
       });
@@ -16,17 +24,15 @@ describe("Project Test", function(){
 
 });
 
-describe("Project Test2", function(){    
-    describe("POST/", function() {    
-        it("returns status code 200", function(done) {     
-            request.post(base_url2, function(error, response, body) {
-            expect(response.statusCode).toBe(200);
-            done();
-
-      });
-
-        });    
-
-    });
-
+describe("Project Test 1", function() {
+  	describe("Status code test suite", function() {
+		for(i in Urls){
+    		it("returns status code 200", function(done) {
+      			request.post(base_url+Urls[i], function(error, response, body) {
+        			expect(response.statusCode).toBe(200);
+					done();
+      			});
+    		});
+		}
+  	});
 });
