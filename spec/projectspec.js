49d8908d1ae343e50d4a8ea5a93148e19e2266c9
mohamed dounnani <mohamed dounnani>
2016-12-14 10:53:01 +0100

    Aggiunti elementi

--- spec/projectspec.js
+++ spec/projectspec.js
@@ -1,12 +1,11 @@
 var request = require("request");
-var base_url = "https://infinite-coast-33422.herokuapp.com/";
-
-var array_base_urls = [base_url+'registrazione' , base_url+'logout', base_url+'register', base_url+'login_home', base_url+'prenotazione'];
+var base_url1 = "https://infinite-coast-33422.herokuapp.com/"
+var base_url2 = "https://infinite-coast-33422.herokuapp.com/registrazione"
 
 describe("Project Test", function(){    
     describe("GET /", function() {   
         it("returns status code 200", function(done) {     
-            request.get(base_url, function(error, response, body) {
+            request.get(base_url1, function(error, response, body) {
             expect(response.statusCode).toBe(200);
             done();
       });
@@ -17,28 +16,17 @@ describe("Project Test", function(){
 
 });
 
-console.log(array_base_urls[0]);
-console.log(array_base_urls[1]);
-console.log(array_base_urls[3]);
-console.log(array_base_urls[4]);
-console.log(array_base_urls[5]);
-console.log(array_base_urls[6]);
-/*describe("Project Test2", function(){    
-            describe("POST/", function() { 
-            for(i=0;i<array_base_urls.length;i++){
-                it("returns status code 200", function(done) {     
-                    request.post(array_base_urls[i], function(error, response, body) {
-                    expect(response.statusCode).toBe(200);
-                    done();
+describe("Project Test2", function(){    
+    describe("POST/", function() {    
+        it("returns status code 200", function(done) {     
+            request.post(base_url2, function(error, response, body) {
+            expect(response.statusCode).toBe(200);
+            done();
 
-              });
+      });
 
-                }); 
-            }
+        });    
 
-            });
+    });
 
-        });*/
-
-
-
+});
