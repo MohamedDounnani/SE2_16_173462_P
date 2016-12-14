d051c7c9e3347000ad621556c302f20d6acd2cf1
mohamed dounnani <mohamed dounnani>
2016-12-14 10:37:35 +0100

    Prova

--- spec/projectspec.js
+++ spec/projectspec.js
@@ -1,22 +1,37 @@
 var request = require("request");
-var base_url = "https://infinite-coast-33422.herokuapp.com/"
+var base_url = "https://infinite-coast-33422.herokuapp.com/";
 
-describe("Project Test", function(){
-    
-    describe("GET /", function() {
-    
-        
-    for(i=0;i<5;i++){
+var array_base_urls = [base_url+'/registrazione' , base_url+'/logout', base_url+'/register', base_url+'/login_home', base_url+'/prenotazione', base_url+'/prenotazione_effettuata' , base_url+'/my_prenotazioni'];
+
+describe("Project Test", function(){    
+    describe("GET /", function() {   
         it("returns status code 200", function(done) {     
             request.get(base_url, function(error, response, body) {
             expect(response.statusCode).toBe(200);
             done();
-
       });
 
-        });
-    }
+    });   
 
     });
 
 });
+
+/*for(i=0;i<7;i++){
+        describe("Project Test2", function(){    
+            describe("POST/", function() {    
+                it("returns status code 200", function(done) {     
+                    request.post(array_base_urls[i], function(error, response, body) {
+                    expect(response.statusCode).toBe(200);
+                    done();
+
+              });
+
+                });    
+
+            });
+
+        });
+}*/
+
+
